1d94795fd0fb78186015ee675b006058400ca5d0
jbuildit <joshuabuilder@gmail.com>
2013-06-26 13:49:52 -0400

    Inital Commit Of Code
    At this point project is only pulling a list of metro area id's for all
    50 states using Songkick API

---
+++ functions.php
@@ -0,0 +1,96 @@
+<?php 
+ $usStates = array(
+  'AL' => 'Alabama',
+  'AK' => 'Alaska',
+  'AZ' => 'Arizona',
+  'AR' => 'Arkansas',
+  'CA' => 'California',
+  'CO' => 'Colorado',
+  'CT' => 'Connecticut',
+  'DE' => 'Delaware',
+  'FL' => 'Florida',
+  'GA' => 'Georgia',
+  'HI' => 'Hawaii',
+  'ID' => 'Idaho',
+  'IL' => 'Illinois',
+  'IN' => 'Indiana',
+  'IA' => 'Iowa',
+  'KS' => 'Kansas',
+  'KY' => 'Kentucky',
+  'LA' => 'Louisiana',
+  'ME' => 'Maine',
+  'MD' => 'Maryland',
+  'MA' => 'Massachusetts',
+  'MI' => 'Michigan',
+  'MN' => 'Minnesota',
+  'MS' => 'Mississippi',
+  'MO' => 'Missouri',
+  'MT' => 'Montana',
+  'NE' => 'Nebraska',
+  'NV' => 'Nevada',
+  'NH' => 'New Hampshire',
+  'NJ' => 'New Jersey',
+  'NM' => 'New Mexico',
+  'NY' => 'New York',
+  'NC' => 'North Carolina',
+  'ND' => 'North Dakota',
+  'OH' => 'Ohio',
+  'OK' => 'Oklahoma',
+  'OR' => 'Oregon',
+  'PA' => 'Pennsylvania',
+  'RI' => 'Rhode Island',
+  'SC' => 'South Carolina',
+  'SD' => 'South Dakota',
+  'TN' => 'Tennessee',
+  'TX' => 'Texas',
+  'UT' => 'Utah',
+  'VT' => 'Vermont',
+  'VA' => 'Virginia',
+  'WA' => 'Washington',
+  'WV' => 'West Virginia',
+  'WI' => 'Wisconsin',
+  'WY' => 'Wyoming',
+);
+
+// Loads All venue IDs for all upcoming events accross the country
+ function loadUpcomingEvents($usStates,$APIKey,$f) { 
+ reset($usStates); //iterare through states
+ while (list($key, $value) = each($usStates)) {
+    // echo" This Is The State: ", $value, "<br>";
+ 	$MetroAreadIDs = "http://api.songkick.com/api/3.0/search/locations.json?query=$value&apikey=$APIKey"; // get the metro area results for the particular state
+ 	$MetroResponse = file_get_contents($MetroAreadIDs); 
+ 	$json_metro =  json_decode($MetroResponse,true);
+ 	$totalEntries= $json_metro['resultsPage']['totalEntries']; //set total number of entries for the state
+ 	//echo "Total Entries: ","<b>",$totalEntries, "<p>";
+	
+	
+ 	for($i=0; $i<=$totalEntries; $i++) {
+ 		$metroID = $json_metro['resultsPage']['results']['location'][$i]['metroArea']['id']; // return a list of venue id's from that state
+		
+ 		echo $metroID, "<-- MetroID  ";
+ 		fwrite($f, $metroID.PHP_EOL); // write to a file
+ 	
+	}
+ }
+} 
+ 
+// Calling Songkick Venue API to load Venue details and zip code data into MYSQL
+
+function getVenueID($metroID, $APIKey){
+
+$Metroupcoming = "http://api.songkick.com/api/3.0/metro_areas/$metroID/calendar.json?apikey=$APIKey";
+$UpcomingResponse = file_get_contents($Venueurl); 
+$json_upcoming =json_decode($UpcomingResponse,true);
+ 
+//NEXT STEP -> Take metro response and parse for venue and zip   
+ 
+print_r( $json_upcoming);
+
+
+
+}
+
+ 
+
+
+?>
